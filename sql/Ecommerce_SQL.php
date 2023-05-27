f2f312ab008e3b1d2e88fedb44a1749632229217
Awaishamza786 <88610384+Awaishamza786@users.noreply.github.com>
2023-05-27 16:40:18 +0500

    update with class

--- sql/Ecommerce_SQL.php
+++ sql/Ecommerce_SQL.php
@@ -88,32 +88,4 @@ try {
   echo "</br>" . $e->getMessage() . "</br>";
 }
 
-
-// if ($db->connect_error) {
-//   die("Connection failed: " . $conn->connect_error);
-// }
-// if (
-//   $db->query("SELECT SCHEMA_NAME
-// FROM INFORMATION_SCHEMA.SCHEMATA
-// WHERE SCHEMA_NAME = '$dbname'")->num_rows < 1
-// ) {
-//   $sql = "create database " . $_POST['db_name'];
-//   try {
-//     $db->query($sql);
-//     echo '</>Database Create!</br>';
-//   } catch (Exception $e) {
-//     echo "</br>Database Creation error</br>" . $e;
-//     exit();
-//   }
-// } else echo "</br>Database already exits</br>";
-
-
-
-// $sql = $_POST['c_query'];
-// try {
-//   if ($db->query($sql))
-//     echo '</br>Table Create!<br>';
-// } catch (Exception $e) {
-//   echo "</br>Table Creation error May be already exits</br>";
-// }
 ?>
